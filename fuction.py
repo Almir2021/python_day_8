b9eaf8ab55ddf1534ffffead4ba4940234d9a0f9
Almir2021 <bosnjicalmir@gmail.com>
2023-09-22 23:22:59 +0200

    some pratice

---
+++ fuction.py
@@ -0,0 +1,24 @@
+# def greet():
+#     print("welcome")
+#     print("welcome")
+#     print("welcome")
+
+# def greet_whit_name(name):
+#     print(f"welcome {name} ")
+#     print(f"How do you do {name}")
+#     print(f"how your day is going {name}")
+
+# greet()
+# greet_whit_name("Almir")
+
+
+def greet_whit(name , location):
+    print(f"welcome {name} ")
+    print(f"How do you do {name}")
+    print(f" you are from {location}")    
+
+#greet_whit("Almir" , "Nowwhere")
+
+
+greet_whit( location= "Nowwhere"  , name= "dave" )
+
