848a6ce7bc3a75671d07ab396530cbdf683d3180
Almir2021 <bosnjicalmir@gmail.com>
2023-09-27 23:52:01 +0200

    Caesar cipher started

---
+++ caesar-cipher.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#Don't change the code above 👆
+count_l = 0
+where_letters = [] 
+
+ 
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_text, shift_amout):
+      plain_text = list(plain_text)
+      print(plain_text)
+      for n in plain_text:
+            for l in alphabet:
+              count_l +=1
+              if n == l :
+                   where_letters.append(count_l)
+      print(where_letters)
+      shift_amout                
+                       
+
+  #TODO-2: Inside the encrypt function, shift each letter of the text forwards in the alphabet by the shift amount and print the encrypted text.  
+  #e.g. 
+  #plain_text = "hello"
+  #shift = 5
+  #cipher_text = "mjqqt"
+  #print output: "The encoded text is mjqqt"
+  
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(plain_text=text, shift_amount=shift)
