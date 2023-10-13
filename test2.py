c41c24cc632e164f0edb5e53f84a5839c1eb69bb
Almir2021 <bosnjicalmir@gmail.com>
2023-10-13 23:06:49 +0200

    making progress i want to finish

--- test2.py
+++ test2.py
@@ -4,32 +4,32 @@ alphabet = caesar_cipher.alphabet
 alphabet = caesar_cipher.alphabet
 direction = 'encode'  # Hardcoded value for testing
 text = 'abc'  # Hardcoded value for testing
-shift_amout = 1  # Hardcoded value for testing
+shift_amout = 5  # Hardcoded value for testing
 
 
-plain_text = "abz"
+plain_text = "hello"
 plain_text = list(plain_text)
 
 shifted_word = []
 where_letters = [] 
-shifted_text = []
-
+shifted_word = []
+shifted_string = ""
 print(plain_text)
 
 for letter in plain_text:
-     where_letters.append(alphabet.index(letter))
-      
+       if letter in plain_text:
+         where_letters.append(alphabet.index(letter))
+
+for l in range(0,len(where_letters)):
+    if where_letters[l] < shift_amout:
+        where_letters[l] += 26
+        where_letters[l] = where_letters[l]-shift_amout
+    else :
+        where_letters[l] = where_letters[l]-shift_amout
+
+    shifted_word.append(alphabet[where_letters[l]])    
+
+cipher_text = shifted_string.join(shifted_word)
+print(f"The encoded text is  {cipher_text}")
+print(type(cipher_text))
 print(where_letters)
-
-for letter in where_letters:
-     if letter < shift_amout:
-         where_letters[letter] = where_letters[ letter] +25 
-
-       # for some reasone index is out of range look in to it. 
-     #where_letters[letter] = where_letters[letter] - shift_amout     
-for letter in where_letters:
-     position = where_letters[letter]
-     shifted_word = position -shift_amout
-     shifted_text += alphabet[shifted_word]
-print(where_letters)        
-print(alphabet[25])  
